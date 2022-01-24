2aa14447ddab75afc8c53d868a3b187416d0e91b
0lgaP <perronolga@gmail.com>
2022-01-24 20:51:58 +0000

    created urls_show and linked it to apt.get in express_server.js

--- express_server.js
+++ express_server.js
@@ -13,10 +13,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls/", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL : req.params.shortURL, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
