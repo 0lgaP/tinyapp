91906aff4b040a33e69d5dbcec7f27e0793961d3
0lgaP <perronolga@gmail.com>
2022-01-24 22:29:01 +0000

    added app.post to server file

--- express_server.js
+++ express_server.js
@@ -4,6 +4,22 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//--------------------------------------------
+function generateRandomString() {
+  let result = "";
+  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklonmopqrstuvwxyz1234567890";
+  for (let i = 0; i < 7; i++){
+    result += char.charAt(Math.floor(Math.random() * char.length))
+  }
+  return result;
+}
+
+
+//--------------------------------------------
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,10 +34,15 @@ app.get("/urls/", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL : req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
 })
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -39,6 +60,17 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+
+//----------------------------------------
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Okkkuuurrrrrrrrr")
+})
+
+//------------------------------------
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
